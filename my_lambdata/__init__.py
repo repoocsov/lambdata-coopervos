86545ec99e8cd5e02c4e27e26df76c44c0224f9b
Cooper Vos <coopervos1@gmail.com>
2020-03-09 17:27:56 -0500

    demonstrate the need to remove code from the global scope of module

--- my_lambdata/__init__.py
+++ my_lambdata/__init__.py
@@ -1,3 +1,4 @@
 # __init__.py
 
 # This file makes the surrounding folder treated as a module. Allows us to import from my_mod.py in my_scripts.py
+# Should use module invocation to run in git bash
--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -5,3 +5,12 @@ def enlarge(n):
     Param n is a number
     '''
     return n * 100
+
+
+
+if __name__ == "__main__":
+    # Only run the code below if this script is invoked from the command-line
+    # not if it is imported from another
+    print("HELLO")
+    y = int(input('Please choose a number'))
+    print(y, enlarge(y))
