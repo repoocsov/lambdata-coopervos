a417ff7cf7c38c8a47a0bdeb433e52193e07e74a
Cooper Vos <coopervos1@gmail.com>
2020-03-09 20:20:42 -0500

    Create two functions

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -7,6 +7,25 @@ def enlarge(n):
     return n * 100
 
 
+def list_to_column(df, li):
+    '''
+    Param df is a dataframe
+    Param li is a list
+    '''
+    import pandas as pd
+
+    x = pd.Series(li)
+    df = pd.concat(df, x, axis=1)
+    return df
+
+
+def return_nulls(df):
+    '''
+    return nulls
+    '''
+    output = df.isnull().sum()
+    return print(output)
+
 
 if __name__ == "__main__":
     # Only run the code below if this script is invoked from the command-line
